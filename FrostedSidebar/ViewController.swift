d23dca16c18cf20e2f1f3ed90b59ad55d313542c
Al Pascual <alpascual@gmail.com>
2014-08-22 17:35:37 -0700

    menu working

--- FrostedSidebar/ViewController.swift
+++ FrostedSidebar/ViewController.swift
@@ -8,74 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController, FrostedSidebarDelegate {
+class ViewController: MasterPage {
     
-	var frostedSidebar: FrostedSidebar = FrostedSidebar(itemImages: [
-			UIImage(named: "gear"),
-			UIImage(named: "globe"),
-			UIImage(named: "profile"),
-			UIImage(named: "star"),
-			UIImage(named: "gear"),
-			UIImage(named: "globe"),
-			UIImage(named: "profile"),
-			UIImage(named: "star"),
-			UIImage(named: "gear"),
-			UIImage(named: "globe"),
-			UIImage(named: "profile"),
-			UIImage(named: "star")],
-		colors: [
-			UIColor(red: 240/255, green: 159/255, blue: 254/255, alpha: 1),
-			UIColor(red: 255/255, green: 137/255, blue: 167/255, alpha: 1),
-			UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
-			UIColor(red: 119/255, green: 152/255, blue: 255/255, alpha: 1),
-			UIColor(red: 240/255, green: 159/255, blue: 254/255, alpha: 1),
-			UIColor(red: 255/255, green: 137/255, blue: 167/255, alpha: 1),
-			UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
-			UIColor(red: 119/255, green: 152/255, blue: 255/255, alpha: 1),
-			UIColor(red: 240/255, green: 159/255, blue: 254/255, alpha: 1),
-			UIColor(red: 255/255, green: 137/255, blue: 167/255, alpha: 1),
-			UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
-			UIColor(red: 119/255, green: 152/255, blue: 255/255, alpha: 1)],
-		selectedItemIndices: NSIndexSet())
+//	var frostedSidebar: FrostedSidebar = FrostedSidebar(itemImages: [
+//			UIImage(named: "gear"),
+//			UIImage(named: "globe"),
+//			UIImage(named: "profile"),
+//			UIImage(named: "star"),
+//			UIImage(named: "gear"),
+//			UIImage(named: "globe"),
+//			UIImage(named: "profile"),
+//			UIImage(named: "star"),
+//			UIImage(named: "gear"),
+//			UIImage(named: "globe"),
+//			UIImage(named: "profile"),
+//			UIImage(named: "star")],
+//		colors: [
+//			UIColor(red: 240/255, green: 159/255, blue: 254/255, alpha: 1),
+//			UIColor(red: 255/255, green: 137/255, blue: 167/255, alpha: 1),
+//			UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
+//			UIColor(red: 119/255, green: 152/255, blue: 255/255, alpha: 1),
+//			UIColor(red: 240/255, green: 159/255, blue: 254/255, alpha: 1),
+//			UIColor(red: 255/255, green: 137/255, blue: 167/255, alpha: 1),
+//			UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
+//			UIColor(red: 119/255, green: 152/255, blue: 255/255, alpha: 1),
+//			UIColor(red: 240/255, green: 159/255, blue: 254/255, alpha: 1),
+//			UIColor(red: 255/255, green: 137/255, blue: 167/255, alpha: 1),
+//			UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
+//			UIColor(red: 119/255, green: 152/255, blue: 255/255, alpha: 1)],
+//		selectedItemIndices: NSIndexSet())
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		frostedSidebar.delegate = self
-		
-		frostedSidebar.calloutsAlwaysSelected = true
-		//			frostedSidebar.showFromRight = true
-		//			frostedSidebara.isSingleSelect = true
-		
-		frostedSidebar.actionForIndex[0] = { self.frostedSidebar.dismissAnimated(true, completion: nil) }
-        frostedSidebar.actionForIndex[1] =
-        {
-            self.performSegueWithIdentifier("FirstSegue", sender: self)
-            self.frostedSidebar.dismissAnimated(true, completion: nil)
-        }
+//		frostedSidebar.delegate = self
+//		
+//		frostedSidebar.calloutsAlwaysSelected = true
+//		//			frostedSidebar.showFromRight = true
+//		//			frostedSidebara.isSingleSelect = true
+//		
+//		frostedSidebar.actionForIndex[0] = { self.frostedSidebar.dismissAnimated(true, completion: nil) }
+//        frostedSidebar.actionForIndex[1] =
+//        {
+//            self.performSegueWithIdentifier("FirstSegue", sender: self)
+//            self.frostedSidebar.dismissAnimated(true, completion: nil)
+//        }
 	}
 	
-	@IBAction func onBurger() {
-        frostedSidebar.showInViewController(self, animated: true)
-    }
-
-    func sidebar(sidebar: FrostedSidebar, willShowOnScreenAnimated animated: Bool){
-
-	}
-    func sidebar(sidebar: FrostedSidebar, didShowOnScreenAnimated animated: Bool){
-        
-    }
-    func sidebar(sidebar: FrostedSidebar, willDismissFromScreenAnimated animated: Bool){
-        
-    }
-    func sidebar(sidebar: FrostedSidebar, didDismissFromScreenAnimated animated: Bool){
-
-	}
-    func sidebar(sidebar: FrostedSidebar, didTapItemAtIndex index: Int){
-        println("tap on item %d", index)
-    }
-    func sidebar(sidebar: FrostedSidebar, didEnable itemEnabled: Bool, itemAtIndex index: Int){
-        
-    }
+//	@IBAction func onBurger() {
+//        frostedSidebar.showInViewController(self, animated: true)
+//    }
+//
+//    func sidebar(sidebar: FrostedSidebar, willShowOnScreenAnimated animated: Bool){
+//
+//	}
+//    func sidebar(sidebar: FrostedSidebar, didShowOnScreenAnimated animated: Bool){
+//        
+//    }
+//    func sidebar(sidebar: FrostedSidebar, willDismissFromScreenAnimated animated: Bool){
+//        
+//    }
+//    func sidebar(sidebar: FrostedSidebar, didDismissFromScreenAnimated animated: Bool){
+//
+//	}
+//    func sidebar(sidebar: FrostedSidebar, didTapItemAtIndex index: Int){
+//        println("tap on item %d", index)
+//    }
+//    func sidebar(sidebar: FrostedSidebar, didEnable itemEnabled: Bool, itemAtIndex index: Int){
+//        
+//    }
     
 }
 
---
+++ HelpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HelpViewController.swift
+//  FrostedSidebar
+//
+//  Created by Al Pascual on 8/22/14.
+//  Copyright (c) 2014 Evan Dekhayser. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: MasterPage {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
