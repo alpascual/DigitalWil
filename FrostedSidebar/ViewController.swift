c059495a07afaf168fc99de6fa177e32654bca06
Al Pascual <alpascual@gmail.com>
2014-08-28 13:57:35 -0700

    a

--- FrostedSidebar/ViewController.swift
+++ FrostedSidebar/ViewController.swift
@@ -8,12 +8,7 @@
 
 import UIKit
 
-class ViewController: MasterPage,UIPickerViewDataSource, UIPickerViewDelegate {
-    
-	
-    @IBOutlet var alertSlider: UISlider!
-    @IBOutlet var timePicker: [UIPickerView]!
-    let timeArray = ["Daily","Monthly","Every 3 Months", "Every 6 Months"]
+class ViewController: MasterPage {
     
 	override func viewDidLoad() {
         
@@ -21,30 +16,10 @@ class ViewController: MasterPage,UIPickerViewDataSource, UIPickerViewDelegate {
         
 		super.viewDidLoad()
         
-        self.timePicker![0].dataSource = self
-        self.timePicker![0].delegate = self
-        
-        // Init the configuration
-        let configuration = Configuration()
         
 	}
     
-    // returns the number of 'columns' to display.
-    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int
-    {
-        return 1
-    }
-    
-    // returns the # of rows in each component..
-    func pickerView(pickerView: UIPickerView!, numberOfRowsInComponent component: Int) -> Int
-    {
-        return self.timeArray.count
-    }
-    
-    func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String!
-    {
-        return self.timeArray[row]
-    }
+
     
 }
 
