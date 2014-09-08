d6615453e21d64b88ef2eed5311041f789bdac8a
Al Pascual <alpascual@gmail.com>
2014-09-08 15:35:30 -0700

    images

--- FrostedSidebar/ViewController.swift
+++ FrostedSidebar/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: MasterPage {
     
+    private var idx: Int = 0
+    private let backGroundArray = [UIImage(named: "img1.jpg"),UIImage(named:"img2.jpg"), UIImage(named: "img3.jpg"), UIImage(named: "img4.jpg")]
     
-    
-    
+    @IBOutlet var backGroundImage: UIImageView!
     @IBOutlet var setupButton: UIButton!
 	override func viewDidLoad() {
         
         viewControllerIndex = 0
         
 		super.viewDidLoad()
+        
+        // Visual Effect View for background
+        var visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.Dark)) as UIVisualEffectView
+        visualEffectView.frame = self.view.frame
+        visualEffectView.alpha = 0.5
+        backGroundImage.image = UIImage(named: "img1.jpg")
+        backGroundImage.addSubview(visualEffectView)
+        
+        
+        NSTimer.scheduledTimerWithTimeInterval(6, target: self, selector: "changeImage", userInfo: nil, repeats: true)
+        
 	}
+    
+    func changeImage(){
+        if idx == backGroundArray.count-1{
+            idx = 0
+        }
+        else{
+            idx++
+        }
+        var toImage = backGroundArray[idx];
+        UIView.transitionWithView(self.backGroundImage, duration: 3, options: .TransitionCrossDissolve, animations: {self.backGroundImage.image = toImage}, completion: nil)
+        
+    }
     
     
     @IBAction func setupButtonPressed(sender: AnyObject) {
